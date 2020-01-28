d658ed163d6b7fd6a99511c13d8ff81cffecf59d
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2020-01-27 21:57:52 -0300

    view deck

--- src/pages/decks/[id].jsx
+++ src/pages/decks/[id].jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
 
 import Layout from '@/components/Layout';
 import DeckContainer from '@/containers/Deck';
 
 import DeckType from '@/types/Deck';
 import CardType from '@/types/Card';
+
+import Cookie from '@/libs/cookie';
+import { restore } from '@/libs/store';
 
 import { getDeck } from '@/services/decks';
 
@@ -20,11 +24,11 @@ Deck.propTypes = {
   cards: PropTypes.arrayOf(CardType).isRequired,
 };
 
-Deck.getInitialProps = async ({ query: { deckId }, res, req }) => {
-  if (!deckId) {
-    res.redirect('/commanders');
-  }
-  const { token } = req.session;
+Deck.getInitialProps = async (ctx) => {
+  const cookies = new Cookie(ctx);
+  const state = await restore({ cookies });
+  const { query: { id: deckId } } = ctx;
+  const token = get(state, 'user.token');
   const { deck, cards } = await getDeck({ deckId, token });
   return { deck, cards };
 };
