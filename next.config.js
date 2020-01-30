16e09057326216edc31a003d33a9958b55b6b606
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2020-01-30 01:38:49 -0300

    task to tomorrou

--- next.config.js
+++ next.config.js
@@ -2,12 +2,8 @@ require('dotenv').config();
 const withCSS = require('@zeit/next-css');
 
 exports.default = withCSS({
-  publicRuntimeConfig: {
-    TUTOR_URL: 'https://tutor.magicnext.com.br',
-  },
   env: {
-    tutor: 'https://tutor.magicnext.com.br',
-    TUTOR_URL: 'https://tutor.magicnext.com.br',
+    TUTOR_URL: process.env.TUTOR_URL,
     API_URL: process.env.API_URL,
   },
   dir: './src',
--- src/services/tutor/index.js
+++ src/services/tutor/index.js
@@ -1,9 +1,6 @@
-import getConfig from 'next/config';
-
 export const details = async ({ name, price = false }) => {
-  const { TUTOR_URL } = process.env;
-  console.log(JSON.stringify(process.env, null, 2), process.env.tutor, 'INFERNO');
-  const url = `${TUTOR_URL}/cards?name=${name}&price=${price}`;
+  const tutor = 'https://tutor.magicnext.com.br';
+  const url = `${tutor}/cards?name=${name}&price=${price}`;
   const req = await fetch(url);
   return req.json();
 };
