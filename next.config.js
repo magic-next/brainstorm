b38a9e3e2347bf4ed24f4d370957e7449927b401
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2020-01-30 01:26:08 -0300

    test hardcoded

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withCSS = require('@zeit/next-css');
 
 exports.default = withCSS({
   publicRuntimeConfig: {
-    TUTOR_URL: process.env.TUTOR_URL,
+    TUTOR_URL: 'https://tutor.magicnext.com.br', // process.env.TUTOR_URL,
   },
   env: {
     API_URL: process.env.API_URL,
--- src/services/tutor/index.js
+++ src/services/tutor/index.js
@@ -2,7 +2,7 @@ import 'isomorphic-fetch';
 
 export const details = async ({ name, price = false }) => {
   const { TUTOR_URL } = process.env;
-  console.log(JSON.stringify(process.env, null, 2), process.env.API_URL);
+  console.log(JSON.stringify(process.env, null, 2), TUTOR_URL);
   const url = `${TUTOR_URL}/cards?name=${name}&price=${price}`;
   const req = await fetch(url);
   return req.json();
