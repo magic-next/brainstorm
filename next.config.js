19ae665be4a5452a3849ef018b8cb0c3d3921fd0
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2020-01-30 01:28:23 -0300

    hardcoded again

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const withCSS = require('@zeit/next-css');
 
 exports.default = withCSS({
   publicRuntimeConfig: {
-    TUTOR_URL: 'https://tutor.magicnext.com.br', // process.env.TUTOR_URL,
+    TUTOR_URL: 'https://tutor.magicnext.com.br',
   },
   env: {
+    TUTOR_URL: 'https://tutor.magicnext.com.br',
     API_URL: process.env.API_URL,
   },
   dir: './src',
