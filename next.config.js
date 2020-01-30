c3230d5770930dd0d5a3389f186d18d69435a5ee
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2020-01-30 01:23:25 -0300

    runtime env

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const withCSS = require('@zeit/next-css');
 
 exports.default = withCSS({
+  publicRuntimeConfig: {
+    TUTOR_URL: process.env.TUTOR_URL,
+  },
   env: {
     API_URL: process.env.API_URL,
-    TUTOR_URL: process.env.TUTOR_URL,
   },
   dir: './src',
 });
--- src/services/tutor/index.js
+++ src/services/tutor/index.js
@@ -1,8 +1,8 @@
 import 'isomorphic-fetch';
 
-const { TUTOR_URL } = process.env;
-
 export const details = async ({ name, price = false }) => {
+  const { TUTOR_URL } = process.env;
+  console.log(JSON.stringify(process.env, null, 2), process.env.API_URL);
   const url = `${TUTOR_URL}/cards?name=${name}&price=${price}`;
   const req = await fetch(url);
   return req.json();
