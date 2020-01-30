8e4deb3363f4eb661c1d49d8e5e241ef1059b74a
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2020-01-30 19:15:11 -0300

    change builds

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ require('dotenv').config();
 const withCSS = require('@zeit/next-css');
 
 exports.default = withCSS({
+  env: {
+    TUTOR_URL: process.env.TUTOR_URL,
+    API_URL: process.env.API_URL,
+  },
   dir: './src',
 });
