4952a318c5bd1001efc751e1121668db60b7ef7f
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2020-03-29 22:23:36 -0300

    use new format

--- src/components/Ranking/RankingPiece/index.jsx
+++ src/components/Ranking/RankingPiece/index.jsx
@@ -4,15 +4,23 @@ import Link from 'next/link';
 import * as S from './styled';
 import RankingType from '../../../types/Ranking';
 
+const getName = (card) => {
+  const fallback = card.name;
+  if (!card.foreign_data) return fallback;
+  if (!card.foreign_data.pt) return fallback;
+  if (!card.foreign_data.pt.name) return fallback;
+  return card.foreign_data.pt.name;
+}
+
 const Piece = ({ ranking }) => {
-  const name = ranking.card.portugueseName || ranking.card.name;
+  const name = getName(ranking);
   return (
     <S.RankingWrapper>
-      <Link href={`/card/${ranking.card.slug}`}>
+      <Link href={`/card/${ranking.slug}`}>
         <a>
           <S.CardWrapper
             version="normal"
-            card={ranking.card}
+            card={ranking}
           />
           <S.NameWrapper>{name}</S.NameWrapper>
           {ranking.count && (
