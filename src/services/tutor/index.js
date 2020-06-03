fd661527fb66fa103b59a92955b04a0e0cf156bc
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2020-06-03 02:34:16 -0300

    remove oversize

--- src/services/tutor/index.js
+++ src/services/tutor/index.js
@@ -4,7 +4,7 @@ export default ({ tutorUrl }) => {
   const request = init({ baseUrl: tutorUrl });
 
   const details = async ({ name, price = false, provider = 'ligamagic' }) => {
-    const url = `/cards?name=${name}&price=${price}&provider=${provider}`;
+    const url = `/cards?name=${name}&price=${price}&provider=${provider}&oversize=0`;
     const req = await request.get(url);
     return req.json();
   };
